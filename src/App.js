a116c16e8d5d0a07dea660a6a5b2bb5d5d164038
kim-ensu <kimdmitriiensu@gmail.com>
2023-01-25 13:20:25 +0600

    Testing class component state

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Dima",
+    };
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn React
-          </a>
+          <p>Hi {this.state.name}</p>
+          <button
+            onClick={() => {
+              this.setState({ name: "Andrei" });
+            }}>
+            Change name
+          </button>
         </header>
       </div>
     );
